05259ec9d94609a2498b3a2cc598857457aea4b0
wxnshw <wender.ribeiro8@gmail.com>
2023-11-13 01:29:32 -0300

    first commit

---
+++ CelularPOO/Models/Nokia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelularPOO.Models
+{
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($@"Instalando o aplicativo {nomeApp} no Nokia.");
+        }
+    }
+}
---
+++ CelularPOO/Program.cs
@@ -0,0 +1,24 @@
+﻿using CelularPOO.Models;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Nokia:");
+            Smartphone smarthphoneNokia = new Nokia(numero: "622102102", modelo: "500", imei: "121a6596252362", memoria: 16);
+            smarthphoneNokia.Ligar();
+            smarthphoneNokia.ReceberLigacao();
+            smarthphoneNokia.InstalarAplicativo("Whatsapp");
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Iphone:");
+            Smartphone smarthphoneIphone = new Iphone(numero: "1541542323", modelo: "6 Plus", imei: "451221220d1211", memoria: 32);
+            smarthphoneIphone.Ligar();
+            smarthphoneIphone.ReceberLigacao();
+            smarthphoneIphone.InstalarAplicativo("Facebook");
+        }
+    }
+}
